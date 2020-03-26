7da015a62ff4e19af9e770402e076a5f01bbf489
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-03-26 23:58:25 +0530

    done problem5

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -13,7 +13,7 @@ import problem5.student.Student;
 public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue myCircularQueue  =new MyCircularQueue(3);
-        myCircularQueue.enqueue(new Student("T",0,1));
+        myCircularQueue.enqueue(new Student("T",0,0));
         myCircularQueue.enqueue(new Student("Y",1,1));
         myCircularQueue.enqueue(new Student("D",1,0));
         for(int i=0;i<myCircularQueue.getSize()+1;i++){
