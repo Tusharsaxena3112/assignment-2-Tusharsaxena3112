cb3f8968dc673e6354902e5f7b8d26b2dd4f940d
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-03-26 23:21:55 +0530

    adding MyCircularQueue.java

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,4 +7,49 @@
 package problem5.circularqueue;
 //to implement circular queue
 public class MyCircularQueue {
+    private int size;
+    private int front;
+    private int rear;
+    private int maxSize;
+    private int[] circularQueue;
+
+    public MyCircularQueue(int maxSize){
+        this.maxSize=maxSize;
+        circularQueue = new int[maxSize];
+        this.front=0;
+        this.rear=0;
+        this.size=0;
+    }
+
+    public void enqueue(int element){
+        if(!isFull()){
+            circularQueue[rear]=element;
+            rear=(rear+1)%maxSize;
+            size++;
+        }
+        else{
+            System.out.println("Queue is full.");
+        }
+    }
+
+    public int dequeue(){
+        int response=-1;
+        if(!isEmpty()){
+            response=circularQueue[front=(front+1)%maxSize];
+            size--;
+            return response;
+        }
+        else{
+            return response;
+        }
+    }
+
+    private boolean isEmpty(){
+        return size==0;
+    }
+
+    private boolean isFull(){
+        return size==maxSize;
+    }
+
 }
